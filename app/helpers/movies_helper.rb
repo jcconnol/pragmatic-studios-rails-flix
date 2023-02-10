231850546228c326ca58cee9ba01914e3aa6b591
John Connolly <jcconnol4@gmail.com>
2023-02-09 20:46:04 -0600

    add editing pages and controller

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,18 @@
 module MoviesHelper
+  def day_and_time(movie)
+    movie.released_on.strftime("%Y")
+  end
+
+  def total_gross(movie)
+
+    if movie.nil?
+      return "nil"
+    end
+
+    if movie.flop?
+      return "Flop!"
+    end
+
+    number_to_currency(movie.total_gross, precision: 0)
+  end
 end
