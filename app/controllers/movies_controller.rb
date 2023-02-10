612dec9f91d8cdbc3e41176a015e64d2507d77eb
John Connolly <jcconnol4@gmail.com>
2023-02-09 21:31:25 -0600

    add movie adding functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,12 +11,26 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def new
+    @movie = Movie.new
+  end
+
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params[:movie].
-      permit(:title, :description, :rating, :released_on, :total_gross)
     @movie.update(movie_params)
 
     redirect_to movie_path(@movie)
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+
+    redirect_to movie_path(@movie)
+  end
+
+  def movie_params
+    params[:movie].
+      permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
