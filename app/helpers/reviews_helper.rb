0732f24c704388a41069fd3bb2538ca1db8d1cb9
John Connolly <jcconnol4@gmail.com>
2023-02-12 10:19:03 -0600

    add average star rating

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, "No reviews")
+    else
+      precise_num = number_with_precision(movie.average_stars, precision: 1)
+      pluralize(precise_num, "star")
+    end
+  end
 end
