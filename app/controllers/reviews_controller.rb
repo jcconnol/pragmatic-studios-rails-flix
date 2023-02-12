b05aeee4abbf4d61b922d032b1e2a082de5731cd
John Connolly <jcconnol4@gmail.com>
2023-02-11 23:14:10 -0600

    add some lessons

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+
+  before_action :get_movie_id
+
+  def index
+    @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+
+    @review = @movie.reviews.new(movie_review_params)
+    if @review.save
+      redirect_to movie_reviews_path(@movie),
+          notice: "Thanks for the review"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def get_movie_id
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def movie_review_params
+    params.require(:review).permit(:stars, :comment, :name)
+  end
 end
